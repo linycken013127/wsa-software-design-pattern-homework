8e5b5e218d2bea5d3882c3f919c62a222763aae2
Ken Lin <linycken210127@gmail.com>
2024-11-09 12:57:38 +0800

    Add select game

--- 2.2.H/2-2/2-2/Program.cs
+++ 2.2.H/2-2/2-2/Program.cs
@@ -2,12 +2,24 @@
 using Game = _2_2.Showdown.Game;
 using Player = _2_2.Showdown.Player;
 
-var p1 = new HumanPlayer();
-var p2 = new AIPlayer();
-var p3 = new AIPlayer();
-var p4 = new AIPlayer();
-List<Player> players = [p1, p2, p3, p4];
-var deck = new Deck();
-var game = new Game(players, deck);
-game.Start();
+Console.WriteLine("選擇遊戲：1. showdown 2. uno");
+var selectGame = Console.ReadLine();
+
+if (selectGame == "1")
+{
+    var p1 = new HumanPlayer();
+    var p2 = new AIPlayer();
+    var p3 = new AIPlayer();
+    var p4 = new AIPlayer();
+    List<Player> players = [p1, p2, p3, p4];
+    var deck = new Deck();
+    var game = new Game(players, deck);
+    game.Start();
+} else if (selectGame == "2")
+{
+    Console.WriteLine("尚未實作");
+} else
+{
+    Console.WriteLine("輸入錯誤");
+}
 
